ab57b54279451d743a695be7f057c1ea291f4466
Louis Viot <louis.viot@epitech.eu>
2020-06-26 22:33:48 +0200

    [ADD] font array definition

---
+++ headers/font.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2019
+** CHIP_8
+** File description:
+** keypad.h
+*/
+
+#ifndef CHIP_8_FONT_H
+#define CHIP_8_FONT_H
+
+#define CHAR_NB     16
+#define CHAR_SIZE   CHAR_NB * 5
+
+extern const unsigned short font[CHAR_SIZE];
+
+#endif //CHIP_8_FONT_H
