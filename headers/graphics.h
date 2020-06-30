ac8577857bb01658baae00c30d51076c19cab565
Louis Viot <louis.viot@epitech.eu>
2020-06-30 18:33:28 +0200

    [ADD] display management

--- headers/graphics.h
+++ headers/graphics.h
@@ -8,4 +8,33 @@
 #ifndef CHIP_8_GRAPHICS_H
 #define CHIP_8_GRAPHICS_H
 
+#include <stdbool.h>
+#include <SDL.h>
+#include "types.h"
+
+#define RATIO           10
+#define CHIP_WIDTH      64
+#define CHIP_HEIGHT     32
+#define WIN_WIDTH       CHIP_WIDTH * RATIO
+#define WIN_HEIGHT      CHIP_HEIGHT * RATIO
+#define FPS             1
+
+struct graphics_s
+{
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    uchar frame_buffer[CHIP_WIDTH * CHIP_HEIGHT];
+    bool render_flag;
+};
+
+typedef struct graphics_s graphics_t;
+
+graphics_t *init_graphics(int width, int height);
+bool catch_quit_event(void);
+void destroy_graphics(void);
+void draw_pixel(ushort x, ushort y);
+void clear_screen(void);
+void render(void);
+void draw_sprite(uchar x, uchar y, uchar mem_addr);
+
 #endif //CHIP_8_GRAPHICS_H
--- srcs/graphics.c
+++ srcs/graphics.c
@@ -5,3 +5,90 @@
 ** graphics.c
 */
 
+#include <string.h>
+#include "chip8.h"
+
+graphics_t *init_graphics(int width, int height)
+{
+    graphics_t *graphics = NULL;
+
+    if (SDL_Init(SDL_INIT_VIDEO)) {
+        return NULL;
+    }
+
+    graphics = malloc(sizeof(graphics_t));
+    if (graphics == NULL) {
+        return NULL;
+    }
+
+    graphics->window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+            width, height, SDL_WINDOW_SHOWN);
+    if (graphics->window == NULL) {
+        return NULL;
+    }
+
+    graphics->renderer = SDL_CreateRenderer(graphics->window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (graphics->renderer == NULL) {
+        return NULL;
+    }
+
+    memset(graphics->frame_buffer, 0, CHIP_WIDTH * CHIP_HEIGHT);
+    graphics->render_flag = false;
+    return graphics;
+}
+
+bool catch_quit_event(void)
+{
+    SDL_Event e;
+
+    while (SDL_PollEvent(&e)) {
+        if (e.type == SDL_QUIT) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void clear_screen(void)
+{
+    chip8_t *chip = get_chip();
+
+    SDL_SetRenderDrawColor(chip->graphics->renderer, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(chip->graphics->renderer);
+}
+
+/*
+ * Actually doesn't draw a pixel, but a square
+ * square size is deduced from screen size/ratio
+ */
+void draw_pixel(ushort x, ushort y)
+{
+    chip8_t *chip = get_chip();
+
+    SDL_Rect fillRect = {(x % CHIP_WIDTH) * RATIO, (y % CHIP_HEIGHT) * RATIO, 1 * RATIO, 1 * RATIO};
+    SDL_SetRenderDrawColor(chip->graphics->renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_RenderFillRect(chip->graphics->renderer, &fillRect);
+
+}
+
+void render(void)
+{
+    chip8_t *chip = get_chip();
+
+    for (uint i = 0; i < CHIP_WIDTH * CHIP_HEIGHT; ++i) {
+        if (chip->graphics->frame_buffer[i]) {
+            draw_pixel(i % CHIP_WIDTH, i / CHIP_WIDTH);
+        }
+    }
+    SDL_RenderPresent(chip->graphics->renderer);
+    SDL_Delay(0);
+}
+
+void destroy_graphics(void)
+{
+    chip8_t *chip = get_chip();
+
+    SDL_DestroyWindow(chip->graphics->window);
+    SDL_DestroyRenderer(chip->graphics->renderer);
+    SDL_Quit();
+}
